39c4a2e930296fc3f5f3883049b5ad8585f16100
S3nsu1k4n <saschakoeppe@gmail.com>
2023-07-29 22:35:09 +0900

    add comment model and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
+  belongs_to :post
   belongs_to :user
-  has_many :comments
 
   validates :title, :body, presence: true
   validates :title, length: { in: 3..16 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :name, :password, presence: true
   validates :name, :password, length: { in: 3..16 }
